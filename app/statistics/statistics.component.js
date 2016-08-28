1d0f6eb039696ca75d94a884f20cc2088260512a
Ghost---Shadow <souradeep.nanda2014@vit.ac.in>
2016-08-28 08:49:32 +0530

    Graphing functional

--- app/statistics/statistics.component.js
+++ app/statistics/statistics.component.js
@@ -1,14 +1,17 @@
 'use strict';
 
 angular.
-  module('statistics').
+  module('statistics', ['chart.js']).
   component('statistics', {
     templateUrl: 'statistics/statistics.template.html',
     controller: function StatisticsController($http) {
       var self = this;
-      $http.get('statistics/statistics.static.json').then(function(response) {
+      $http.get('statistics/statistics.static.json').then(function (response) {
         self.statistics = response.data;
+        self.labels = self.statistics.labels;
+        self.data = self.statistics.data;
       });
 
+      self.analysis = ["Estimated time placeholder","Actual time placeholder"];
     }
   });
