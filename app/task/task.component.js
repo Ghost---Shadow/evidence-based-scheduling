8788d7b80c9bedeaa7bc9e3424899a827716e9d4
Ghost---Shadow <souradeep.nanda2014@vit.ac.in>
2016-08-30 22:04:47 +0530

    CRUD implemented

--- app/task/task.component.js
+++ app/task/task.component.js
@@ -10,34 +10,49 @@ angular.
       self.analysis = {};
       self.delay = 1000;
       self.velocities = [];
-      self.bins = [.2,.4,.6,.8,1.0,1.2,1.4,1.6,1.8,2.0];
+      self.bins = [.2, .4, .6, .8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0];
       self.xAxis = [];
-      self.yAxis = [[]];      
+      self.yAxis = [[]];
       self.binsCount = self.bins.length;
       self.binSize = .2;
 
-      // TODO: Load tasks from mongodb
-      $http.get('/tasks').then(function (response) {
-        self.tasks = response.data;
-        //console.log(self.tasks);
-      });
+      self.refreshList = function () {
+        $http.get('/tasks').then(function (response) {
+          self.tasks = response.data;
+          function compare(a, b) {
+            var val1 = parseInt(a.order);
+            var val2 = parseInt(a.order);
+            if (val1 < val2)
+              return -1;
+            if (val1 > val2)
+              return 1;
+            return 0;
+          }
+          //self.tasks.sort(compare);
+          //console.log(self.tasks);
+        });
+      }
+      self.refreshList();
 
       // Add a new task
       self.addTask = function () {
         var newTask = {
-          "name": "",
-          "estimated": NaN,
+          "name": "New",
+          "estimated": 0.0,
           "actual": 0.0,
-          "order":tasks.length
+          "order": self.tasks.length
         };
         self.tasks.push(newTask);
-        // TODO: Add task to database
+        var getRequest = '/addTask/' + newTask.name + '/' + newTask.estimated + '/' + newTask.actual + '/' + newTask.order;
+        $http.get(getRequest).then(function (response) {
+          self.refreshList();
+        });
       }
 
       // TODO: Optimize
-      self.findIndexForId = function(_id){
-        for(var i = 0; i < self.tasks.length; i++){
-          if(self.tasks[i]._id == _id) break;
+      self.findIndexForId = function (_id) {
+        for (var i = 0; i < self.tasks.length; i++) {
+          if (self.tasks[i]._id == _id) break;
         }
         return i;
       }
@@ -47,15 +62,34 @@ angular.
         var index = self.findIndexForId(_id);
         self.isTimerRunning.splice(index, 1);
         self.tasks.splice(index, 1);
-        // TODO: Remove task from database
+        var getRequest = /removeTask/ + _id;
+        $http.get(getRequest).then(function (response) {
+          self.refreshList();
+        });
       }
 
       // Move task up or down
-      self.changeOrder = function(_id,delta){
+      self.changeOrder = function (_id, delta) {
         var index = self.findIndexForId(_id);
         var temp = self.tasks[index];
         self.tasks.splice(index, 1);
-        self.tasks.splice(index-delta, 0, temp);
+        self.tasks.splice(index - delta, 0, temp);
+        for (var i = 0; i < self.tasks.length; i++) {
+          self.tasks[i].order = i;
+        }
+        for (var i = 0; i < self.tasks.length; i++) {
+          //self.update(self.tasks[i]._id);
+        }
+        //self.refreshList();
+      }
+
+      self.update = function (_id) {
+        var index = self.findIndexForId(_id);
+        var task = self.tasks[index];
+        var getRequest = '/updateTask/' + task._id + '/' + task.name + '/' + task.estimated + '/' + task.actual + '/' + task.order;
+        $http.get(getRequest).then(function (response) {
+          //self.refreshList();
+        });
       }
 
       // Toggle the state of timer
@@ -65,6 +99,7 @@ angular.
           self.isTimerRunning[index] = true;
         } else {
           self.isTimerRunning[index] = false;
+          self.update(_id);
         }
       }
 
@@ -85,7 +120,7 @@ angular.
         self.velocities = [];
         for (var i = 0; i < self.tasks.length; i++) {
           var task = self.tasks[i];
-          if(task.actual > 0)
+          if (task.actual > 0)
             self.velocities[i] = task.estimated / task.actual;
         }
       }
@@ -103,28 +138,28 @@ angular.
           estimatedTime += parseFloat(task.estimated);
         }
 
-        self.analysis.estimatedTime = "Estimated Time: " + estimatedTime/60 + " hours";
-        self.analysis.elapsedTime = "Elapsed Time: " + elapsedTime/60 + " hours";        
+        self.analysis.estimatedTime = "Estimated Time: " + estimatedTime / 60 + " hours";
+        self.analysis.elapsedTime = "Elapsed Time: " + elapsedTime / 60 + " hours";
         self.updateVelocities();
 
-        var frequency = [];        
+        var frequency = [];
         var cumulativeFrequency = [];
         var n = self.velocities.length;
-        for(var i = 0; i < self.binsCount; i++){
+        for (var i = 0; i < self.binsCount; i++) {
           frequency[i] = 0;
           cumulativeFrequency[i] = 0;
         }
 
-        for(var i = 0; i < n; i++)
+        for (var i = 0; i < n; i++)
           frequency[self.transfer(self.velocities[i])]++;
-        
+
         //console.log(frequency);
         frequency[0] /= n;
         cumulativeFrequency[0] = frequency[0];
         //self.yAxis[0][0] = frequency[0];
-        for(var i = 1; i < self.binsCount; i++){
+        for (var i = 1; i < self.binsCount; i++) {
           frequency[i] /= n;
-          cumulativeFrequency[i] = cumulativeFrequency[i-1] + frequency[i];          
+          cumulativeFrequency[i] = cumulativeFrequency[i - 1] + frequency[i];
           self.xAxis[i] = parseFloat((estimatedTime * self.bins[i] / 60).toFixed(3));
           self.yAxis[0][i] = cumulativeFrequency[i] * 100;
         }
@@ -132,8 +167,8 @@ angular.
         //console.log(self.yAxis[0]);
       }
 
-      self.transfer = function(velocity){
-        return parseInt(velocity/self.binSize)-1;
+      self.transfer = function (velocity) {
+        return parseInt(velocity / self.binSize) - 1;
       }
     }
   }).filter('StartStopFilter', function () {
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.Promise = global.Promise;
 
 // connect to MongoDB
 mongoose.connect('mongodb://localhost/EbsMonteCarlo')
-  .then(() =>  console.log('Connection succesful'))
+  .then(() => console.log('Connection succesful'))
   .catch((err) => console.error(err));
 
 // Load the Task module
@@ -31,3 +31,47 @@ app.get('/tasks', function (req, res, next) {
     //console.log(tasks);
   });
 });
+
+// /addTask/NewTask/120/120/7
+app.get('/addTask/:name/:estimated/:actual/:order', function (req, res, next) {
+  var newTask = new Task({
+    "name": req.params["name"],
+    "estimated": req.params["estimated"],
+    "actual": req.params["actual"],
+    "order": req.params["order"]
+  });
+  newTask.save(function (err) {
+    if (err) return console.log(err);
+  })
+  res.send("Done");
+});
+
+app.get('/removeTask/:_id', function (req, res, next) {
+  console.log("Removing " + req.params["_id"]);
+  var msg = "";
+  Task.remove({ "_id": req.params["_id"] }, function (err) {
+    if (err) msg = err;
+    else msg = "Done";
+  });
+  console.log(msg);
+  res.send(msg);
+});
+
+app.get('/updateTask/:_id/:name/:estimated/:actual/:order', function (req, res, next) {
+  console.log("Updating " + req.params["_id"]);
+  var msg = "";
+  var query = { "_id": req.params["_id"] };
+  var action = {
+    "name": req.params["name"],
+    "estimated": req.params["estimated"],
+    "actual": req.params["actual"],
+    "order": req.params["order"]
+  };
+  Task.findOneAndUpdate(query,action, function (err) {
+    if (err) msg = err;
+    else msg = "Done";
+  });
+  console.log(msg);
+  res.send(msg);
+});
+
